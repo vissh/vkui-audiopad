67a2696b441dd008961bbcc4cd84c56c3ef2316a
Denis Matveev <d.matveev@corp.mail.ru>
2022-12-11 15:02:08 +0300

    tab spaces

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import "@vkontakte/vkui/dist/vkui.css";
 
 import {
-  AdaptivityProvider, AppRoot, ConfigProvider, Panel, Platform, SplitCol,
-  SplitLayout, View, WebviewType
+    AdaptivityProvider, AppRoot, ConfigProvider, Panel, Platform, SplitCol,
+    SplitLayout, View, WebviewType
 } from "@vkontakte/vkui";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
@@ -12,32 +12,32 @@ import { ContentPanel } from "./components/ContentPanel";
 import { store } from "./store/store";
 
 const App = () => {
-  return (
-    <ConfigProvider
-      platform={Platform.ANDROID}
-      webviewType={WebviewType.INTERNAL}
-    >
-      <AdaptivityProvider>
-        <Provider store={store}>
-          <AppRoot>
-            <SplitLayout>
-              <SplitCol>
-                <View activePanel="vkaudiopad">
-                  <Panel id="vkaudiopad">
-                    <AudioControls />
-                    <ContentPanel />
-                  </Panel>
-                </View>
-              </SplitCol>
-            </SplitLayout>
-          </AppRoot>
-        </Provider>
-      </AdaptivityProvider>
-    </ConfigProvider>
-  );
+    return (
+        <ConfigProvider
+            platform={Platform.ANDROID}
+            webviewType={WebviewType.INTERNAL}
+        >
+            <AdaptivityProvider>
+                <Provider store={store}>
+                    <AppRoot>
+                        <SplitLayout>
+                            <SplitCol>
+                                <View activePanel="vkaudiopad">
+                                    <Panel id="vkaudiopad">
+                                        <AudioControls />
+                                        <ContentPanel />
+                                    </Panel>
+                                </View>
+                            </SplitCol>
+                        </SplitLayout>
+                    </AppRoot>
+                </Provider>
+            </AdaptivityProvider>
+        </ConfigProvider>
+    );
 };
 
 ReactDOM.render(
-  <App />,
-  document.getElementById("root")
+    <App />,
+    document.getElementById("root")
 );
