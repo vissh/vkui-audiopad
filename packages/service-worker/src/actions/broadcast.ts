fca34cbb3b3093c24dc394f606291aa761d5014d
elfwy-uwu <norman74@list.ru>
2025-12-31 01:38:27 +0500

    Fix status not updating

--- packages/service-worker/src/actions/broadcast.ts
+++ packages/service-worker/src/actions/broadcast.ts
@@ -1,9 +1,9 @@
-import { vkApiClient } from '@vk-audiopad/common'
+import { vkApiClient, type commonTypes } from '@vk-audiopad/common'
 import { applicationState } from '../state'
 
-export const onBroadcast = async (enabled: boolean): Promise<void> => {
+export const onBroadcast = async (enabled: boolean, trackOverride?: commonTypes.TrackItem): Promise<void> => {
     if (enabled) {
-        const track = applicationState.activeTrack
+        const track = trackOverride ?? applicationState.activeTrack
         if (track != null) {
             const ownerId = track.id.split('_')[0]
             const audioId = track.id.split('_')[1]
--- packages/service-worker/src/listeners/storage.ts
+++ packages/service-worker/src/listeners/storage.ts
@@ -5,10 +5,17 @@ import { onBroadcast } from '../actions/broadcast'
 import { updateVolume } from '../offscreen/update-volume'
 import { storage } from '../storage'
 
+import { applicationState } from '../state'
+
 export const startListeningStorageEvents = (): void => {
   storage.durationMode.listen(onDurationMode)
   storage.volume.listen((value) => { void updateVolume(value ?? 0) })
   storage.played.listen((played) => { void onPlayed(played) })
   storage.shuffle.listen((shuffle) => { void onShuffle(shuffle) })
   storage.castToStatus.listen((castToStatus) => { void onBroadcast(castToStatus ?? false) })
+  storage.activeTrack.listen((activeTrack) => {
+    if (applicationState.castToStatus && activeTrack != null) {
+      void onBroadcast(true, activeTrack)
+    }
+  })
 }
